ab19292842d38408292c29f28285bf955e601cd8
juan pablo <juan.jaramillo62@udea.edu.co>
2020-03-01 21:04:24 -0500

    machete

--- src/main.cpp
+++ src/main.cpp
@@ -11,41 +11,57 @@
 BluetoothSerial SerialBT;
 HMC5883L magnetometro;
 int16_t mx, my, mz;
+int16_t valorEstable;
+int16_t valorEstableOcupado;
+int16_t margen = 25;
+int16_t ultimosValores[5];
+int16_t recorrido = 0;
 int16_t magnitud2 = 0;
-uint8_t estado;
+bool estado;
+uint8_t imprecion;
+
 bool salida;
 void setup() {
     
     Serial.begin(9600);
+    pinMode(0,OUTPUT);
+    digitalWrite(0,LOW);
     Serial.println("Inicializando Magnetometro...");
     SerialBT.begin("ESP32test"); //Bluetooth device name
     Serial.println("The device started, now you can pair it with bluetooth!");
     //Inicializamos la comunicación I2C y el magnetómetro
     Wire.begin();
     magnetometro.initialize();
+    delay(5000);
+    valorEstable = magnetometro.getHeadingZ();
+    for (size_t i = 0; i < sizeof(ultimosValores) - 1; i++)
+    {
+        ultimosValores[i] = valorEstable;
+    }
+    
+    
+}
+
+int promedio(int16_t a[]){
+    int suma;
+    for (size_t i = 0; i < sizeof(a); i++)
+    {
+        suma += a[i];
+    }
+    return suma/sizeof(a);
 }
 
 void loop() {
     //Obtenemos del magnetometro las componentes del campo magnético
     magnetometro.getHeading(&mx, &my, &mz);
     int16_t magnitud = sqrt( pow(mx,2) + pow(my,2) + pow(mz,2) );
-    /*
-    Serial.print("mx:");
-    Serial.print(mx); 
-    Serial.print("\tmy:");
-    Serial.print(my);
-    Serial.print("\tmz:");
-    Serial.print(mz);
-    Serial.print("\tmagnitud: ");
-    */
-    
     //Serial.println(magnitud);
     if (SerialBT.available())
     {      
-        estado = SerialBT.read();
-        Serial.println(estado);
+        imprecion = SerialBT.read();
+        Serial.println(imprecion);
     }
-    switch (estado)
+    switch (imprecion)
     {
     case 100:
         Serial.println(magnitud);
@@ -61,18 +77,55 @@ void loop() {
         Serial.print(mx);
         SerialBT.println(mx);
         break;
+
     case 121:
         Serial.print(my);
         SerialBT.println(my);
         break;
+
     case 122:
         Serial.print(mz);
         SerialBT.println(mz);
         break;
+
     default:
         break;
     }
     magnitud2=magnitud;
+
     delay(300);
+    ultimosValores[recorrido] = mz;
+    recorrido = (recorrido+1)%sizeof(ultimosValores);
+
+    if ((promedio(ultimosValores) < valorEstable-margen) && !estado)
+    {
+        delay(2000);
+        if (magnetometro.getHeadingZ < valorEstable-margen)
+        {
+            estado = true;
+            digitalWrite(0,HIGH);
+            delay(2000);
+            valorEstableOcupado = magnetometro.getHeadingZ;
+
+        }
+        
+    }
+    if (estado &&  (promedio(ultimosValores) > valorEstableOcupado + margen))
+    {
+        delay(1000);
+        if ((magnetometro.getHeadingZ < valorEstable+margen) && (magnetometro.getHeadingZ > valorEstable-margen))
+        {
+            estado = false;
+            digitalWrite(0,LOW);
+        }
+        
+    }
+    
+    
+
+
+
+
+
 
 }
